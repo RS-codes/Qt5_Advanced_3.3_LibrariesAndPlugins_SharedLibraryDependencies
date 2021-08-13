cc984e39d82f16586c83b8b64f403c91f9a23ca1
RS-codes <RS-codes@github.com>
2021-08-13 09:41:02 +0530

    SourceCode added

---
+++ myapp/main.cpp
@@ -0,0 +1,15 @@
+#include <QCoreApplication>
+#include <QDebug>
+
+#include "../widget/widget_global.h"
+#include "../widget/widget.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    Widget widget;
+    widget.start();
+
+    return a.exec();
+}
